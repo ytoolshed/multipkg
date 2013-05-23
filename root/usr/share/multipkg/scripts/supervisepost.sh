7d3b6c1b14762f51450bf9556acfe36088a8f05e
Jianing Yang <jianingy.yang@gmail.com>
2013-05-23 11:26:47 +0800

    add search path for command svc

--- root/usr/share/multipkg/scripts/supervisepost.sh
+++ root/usr/share/multipkg/scripts/supervisepost.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
 chown nobody /etc/service/%service%/log/main
 ln -sfn /etc/service/%service% /service/%service%
-/usr/local/bin/svc -t /service/%service% /service/%service%/log
+svc -t /service/%service% /service/%service%/log
 
